33621da27bf3d948bed7e914cbff6448ec7e86d7
MasakiYoshida39 <149563224+MasakiYoshida39@users.noreply.github.com>
2025-05-30 19:46:22 +0900

    ウィンドウに文字を表示させる

--- src/test/WebWindow.java
+++ src/test/WebWindow.java
@@ -1,6 +1,9 @@
 package test;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Frame;
+import java.awt.Label;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -13,10 +16,24 @@ public class WebWindow {
 
 // Frame を継承したクラス
 class Sample1 extends Frame {
+    // フィールドとして宣言（←これが必要）
+    private Label lb;
+
     
     public Sample1() {
     	//ウィンドウのタイトル
         super("test");
+        
+        //ラベルの作成
+        lb = new Label("sam");
+        //ラベルの表示
+        add(lb);
+        
+        //文字色を変更
+        lb.setForeground(Color.blue);
+        //"serif"：フォントの種類（書体）　Font.BOLD：フォントのスタイル　34：フォントサイズ（ピクセル）
+        lb.setFont(new Font("serif",Font.BOLD,34));
+        
         
         addWindowListener(new SampleWindowListener());
         //ウィンドウのサイズと表示
